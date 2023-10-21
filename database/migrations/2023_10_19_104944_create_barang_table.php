7a5b15ff19c95fe165774898f3c405d6c62f9393
StevenSutantoh <steven.sutantoh@gmail.com>
2023-10-21 13:57:41 +0700

    add database

---
+++ database/migrations/2023_10_19_104944_create_barang_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('barang', function (Blueprint $table) {
+            $table->increments('id_barang');
+            $table->integer('id_kategori');
+            $table->integer('id_stok');
+            $table->string('nama_barang')->unique();
+            $table->string('merk')->nullable();
+            $table->integer('kuantitas');
+            $table->integer('harga_jual');
+            $table->integer('stok');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('barang');
+    }
+};
