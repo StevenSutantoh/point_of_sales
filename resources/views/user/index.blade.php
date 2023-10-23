e8296392f1c9184eb0f76c174cfcb27e64f35e97
StevenSutantoh <steven.sutantoh@gmail.com>
2023-10-24 02:26:53 +0700

    add user

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 @section('title', 'User Info')
 
-{{-- @section('content')
+@section('content')
     <div class="container">
         <div class="row">
 
@@ -9,60 +9,31 @@
                 <div class="card">
                     <div class="card-header">User Info</div>
                     <div class="card-body">
-                        <a href="{{ url('/user/create') }}" class="btn btn-success btn-sm" title="Add New userRole">
-                            <i class="fa fa-plus" aria-hidden="true"></i> Add New
-                        </a>
-
-                        <form method="GET" action="{{ url('/user') }}" accept-charset="UTF-8" class="form-inline my-2 my-lg-0 float-right" role="search">
-                            <div class="input-group">
-                                <input type="text" class="form-control" name="search" placeholder="Search..." value="{{ request('search') }}">
-                                <span class="input-group-append">
-                                    <button class="btn btn-secondary" type="submit">
-                                        <i class="fa fa-search"></i>
-                                    </button>
-                                </span>
-                            </div>
-                        </form>
-
-                        <br/>
-                        <br/>
-                        <div class="table-responsive">
-                            <table class="table">
-                                <thead>
+                        <div class="container">
+                            <table height="150px" width="600px" border="1">
+                                <tr>
+                                    <td>Nama</td>
+                                    <td>Email</td>
+                                </tr>
+                    
+                                @foreach ($data as $d)
                                     <tr>
-                                        <th>#</th><th>Name</th><th>Actions</th>
+                                        <td>{{ $d->name }}</td>
+                                        <td>{{ $d->email }}</td>
                                     </tr>
-                                </thead>
-                                <tbody>
-                                @foreach($user as $item)
-                                    <tr>
-                                        <td>{{ $loop->iteration }}</td>
-                                        <td>{{ $item->name }}</td>
-                                        <td>
-                                            <a href="{{ url('/user/' . $item->id) }}" title="View userRole"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/user/' . $item->id . '/edit') }}" title="Edit userRole"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-
-                                            <form method="POST" action="{{ url('/user' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete userRole" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                            </form>
-                                        </td>
-                                    </tr>
-                                @endforeach
-                                </tbody>
+                                    @endforeach
                             </table>
-                            <div class="pagination-wrapper"> {!! $userrole->appends(['search' => Request::get('search')])->render() !!} </div>
-                        </div>
+                        </div>    
 
+                        
                     </div>
                 </div>
             </div>
         </div>
     </div>
-@endsection --}}
+@endsection
 
-@section('content')
+{{-- @section('content')
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -88,4 +59,4 @@
         </table>
     </div>    
 </body>
-</html>
+</html> --}}
