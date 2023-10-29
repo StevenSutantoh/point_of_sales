53d23675dcca587a7d84f2e0c017e871c7c102c4
ravelens <andhikaravelens99@gmail.com>
2023-10-29 20:29:30 +0700

    =Add Roles and Change Register method

--- app/Http/Controllers/BarangController.php
+++ app/Http/Controllers/BarangController.php
@@ -7,14 +7,28 @@ use App\Models\Kategori;
 use Illuminate\Http\Request;
 use Validator;
 use App\Http\Controllers\Controller;
-use Spatie\Permission\Models\Role;
-use Spatie\Permission\Models\Permission;
+// use Spatie\Permission\Models\Role;
+// use Spatie\Permission\Models\Permission;
 use DB;
 use Illuminate\View\View;
 use Illuminate\Http\RedirectResponse;
 
 class BarangController extends Controller
 {
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    function __construct()
+    {
+         $this->middleware('permission:product-list|product-create|product-edit|product-delete', ['only' => ['index','show']]);
+         $this->middleware('permission:product-create', ['only' => ['create','store']]);
+         $this->middleware('permission:product-edit', ['only' => ['edit','update']]);
+         $this->middleware('permission:product-delete', ['only' => ['destroy']]);
+    }
+
     /**
      * Display a listing of the resource.
      */
