c575fb680e174406b559e0fe0ad3ca48c519966f
ravelens <andhikaravelens99@gmail.com>
2023-11-02 02:00:58 +0700

    =Fix Username Display

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -19,15 +19,19 @@
           <li class="dropdown user user-menu">
             <a href="#" class="dropdown-toggle" data-toggle="dropdown">
               <img src="{{ asset('AdminLTE-2/dist/img/user2-160x160.jpg') }}" class="user-image" alt="User Image">
-              {{-- <span class="hidden-xs">{{ $user->name }}</span> --}}
+              <span>
+                {{ Auth::user()->name }}
+              </span>
             </a>
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
                 <img src="{{ asset('AdminLTE-2/dist/img/user2-160x160.jpg') }}" class="img-circle" alt="User Image">
-
                 <p>
-                  {{-- {{ $user->name }} --}}
+                  {{ Auth::user()->name }}
+                </p>
+                <p>
+                  {{ Auth::user()->email }}
                 </p>
               </li>
               <!-- Menu Footer-->
